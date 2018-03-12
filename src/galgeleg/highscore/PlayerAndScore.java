a593d7b7de0054194253ebe3d3ffad0f73183b09
JanusAA <janus.andreasson@hotmail.com>
2018-03-12 16:24:17 +0100

    Added highscore - pizzatime?

---
+++ src/galgeleg/highscore/PlayerAndScore.java
@@ -0,0 +1,43 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package galgeleg.highscore;
+
+import java.util.Comparator;
+
+/**
+ *
+ * @author janus
+ */
+public class PlayerAndScore{
+    private int highscore;
+    private String studentNumber;
+    
+    public  PlayerAndScore(int highscore, String studentNumber){
+        this.highscore = highscore;
+        this.studentNumber = studentNumber;
+        
+        
+    }
+    public int getHighscore(){
+        return this.highscore;
+    }
+    
+    public String getStudentNumber(){
+        return this.studentNumber;
+    }
+    
+    public static Comparator<PlayerAndScore> playerScore = new Comparator<PlayerAndScore>(){
+        @Override
+        public int compare(PlayerAndScore p1, PlayerAndScore p2){
+            int highscore1 = p1.getHighscore();
+            int highscore2 = p2.getHighscore();
+            
+            return highscore2-highscore1;
+        }
+        
+    };
+}
+
