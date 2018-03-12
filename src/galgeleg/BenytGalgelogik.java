34df6f6ba980a801fd089280f647dd95bdd81f9a
Kemikaze <aleksander.wm@gmail.com>
2018-03-12 13:11:49 +0100

    OK

--- src/galgeleg/BenytGalgelogik.java
+++ src/galgeleg/BenytGalgelogik.java
@@ -8,6 +8,7 @@ import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 public class BenytGalgelogik {
+    //test til om janus lugter
 
   public static void main(String[] args) throws MalformedURLException{
     URL url = new URL("http://ubuntu4.saluton.dk:9950/Galgelogik?wsdl");
