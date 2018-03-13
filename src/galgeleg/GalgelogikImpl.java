dbfc1ade1824596ae84462d04549c7d058e795a5
JanusAA <janus.andreasson@hotmail.com>
2018-03-13 15:40:04 +0100

    Added getWordsFromDR, which gets words from the frontpage of DR's webpage, and strips the HTML tags.

--- src/galgeleg/GalgelogikImpl.java
+++ src/galgeleg/GalgelogikImpl.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.jws.WebService;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
@@ -22,146 +24,194 @@ import javax.xml.ws.Service;
 public class GalgelogikImpl implements GalgelogikI {
     
     
-  /** AHT afprøvning er muligeOrd synlig på pakkeniveau */
-  ArrayList<String> muligeOrd = new ArrayList<String>();
-  private String ordet;
-  private ArrayList<String> brugteBogstaver = new ArrayList<String>();
-  private String synligtOrd;
-  private int antalForkerteBogstaver, points;
-  private boolean sidsteBogstavVarKorrekt;
-  private boolean spilletErVundet;
-  private boolean spilletErTabt;
-  
-  public GalgelogikImpl() {
-    muligeOrd.add("bil");
-    muligeOrd.add("computer");
-    muligeOrd.add("programmering");
-    muligeOrd.add("motorvej");
-    muligeOrd.add("busrute");
-    muligeOrd.add("gangsti");
-    muligeOrd.add("skovsnegl");
-    muligeOrd.add("solsort");
-    muligeOrd.add("seksten");
-    muligeOrd.add("sytten");
-    nulstil();
-  }
-
-
-  public ArrayList<String> getBrugteBogstaver() {
-    return brugteBogstaver;
-  }
-
-  public String getSynligtOrd() {
-    return synligtOrd;
-  }
-
-  public String getOrdet() {
-    return ordet;
-  }
-
-  public int getAntalForkerteBogstaver() {
-    return antalForkerteBogstaver;
-  }
-
-  public boolean erSidsteBogstavKorrekt() {
-    return sidsteBogstavVarKorrekt;
-  }
-
-  public boolean erSpilletVundet() {
-    return spilletErVundet;
-  }
-
-  public boolean erSpilletTabt() {
-    return spilletErTabt;
-  }
-
-  public boolean erSpilletSlut() {
-    return spilletErTabt || spilletErVundet;
-  }
-  public int getPoints(){
-      return points;
-  }
-
-  public void nulstil() {
-    brugteBogstaver.clear();
-    antalForkerteBogstaver = 0;
-    //Nulstiller points i terminal version.
-    points = 0;
-    spilletErVundet = false;
-    spilletErTabt = false;
-    ordet = muligeOrd.get(new Random().nextInt(muligeOrd.size()));
-    opdaterSynligtOrd();
-  }
-
-
-  public void opdaterSynligtOrd() {
-    synligtOrd = "";
-    spilletErVundet = true;
-    for (int n = 0; n < ordet.length(); n++) {
-      String bogstav = ordet.substring(n, n + 1);
-      if (brugteBogstaver.contains(bogstav)) {
-        synligtOrd = synligtOrd + bogstav;
-      } else {
-        synligtOrd = synligtOrd + "*";
+    /** AHT afprøvning er muligeOrd synlig på pakkeniveau */
+    ArrayList<String> muligeOrd = new ArrayList<String>();
+    private String ordet;
+    private ArrayList<String> brugteBogstaver = new ArrayList<String>();
+    private String synligtOrd;
+    private int antalForkerteBogstaver, points;
+    private boolean sidsteBogstavVarKorrekt;
+    private boolean spilletErVundet;
+    private boolean spilletErTabt;
+    
+    public GalgelogikImpl() {
+        muligeOrd.add("bil");
+        muligeOrd.add("computer");
+        muligeOrd.add("programmering");
+        muligeOrd.add("motorvej");
+        muligeOrd.add("busrute");
+        muligeOrd.add("gangsti");
+        muligeOrd.add("skovsnegl");
+        muligeOrd.add("solsort");
+        muligeOrd.add("seksten");
+        muligeOrd.add("sytten");
+        nulstil();
+    }
+    
+    
+    public ArrayList<String> getBrugteBogstaver() {
+        return brugteBogstaver;
+    }
+    
+    public String getSynligtOrd() {
+        return synligtOrd;
+    }
+    
+    public String getOrdet() {
+        return ordet;
+    }
+    
+    public int getAntalForkerteBogstaver() {
+        return antalForkerteBogstaver;
+    }
+    
+    public boolean erSidsteBogstavKorrekt() {
+        return sidsteBogstavVarKorrekt;
+    }
+    
+    public boolean erSpilletVundet() {
+        return spilletErVundet;
+    }
+    
+    public boolean erSpilletTabt() {
+        return spilletErTabt;
+    }
+    
+    public boolean erSpilletSlut() {
+        return spilletErTabt || spilletErVundet;
+    }
+    public int getPoints(){
+        return points;
+    }
+    public void clearPoints(){
+        this.points = 0;
+    }
+    
+    public void nulstil() {
+        brugteBogstaver.clear();
+        antalForkerteBogstaver = 0;
+        //Nulstiller points i terminal version.
+        points = 0;
         spilletErVundet = false;
-      }
+        spilletErTabt = false;
+        ordet = muligeOrd.get(new Random().nextInt(muligeOrd.size()));
+        opdaterSynligtOrd();
     }
-  }
-
-  public void gætBogstav(String bogstav) {
-    if (bogstav.length() != 1) return;
-    System.out.println("Der gættes på bogstavet: " + bogstav);
-    if (brugteBogstaver.contains(bogstav)) return;
-    if (spilletErVundet || spilletErTabt) return;
-
-    brugteBogstaver.add(bogstav);
-
-    if (ordet.contains(bogstav)) {
-      sidsteBogstavVarKorrekt = true;
-      System.out.println("Bogstavet var korrekt: " + bogstav);
-      points++;
-    } else {
-      // Vi gættede på et bogstav der ikke var i ordet.
-      sidsteBogstavVarKorrekt = false;
-      System.out.println("Bogstavet var IKKE korrekt: " + bogstav);
-      antalForkerteBogstaver = antalForkerteBogstaver + 1;
-      if (antalForkerteBogstaver > 6) {
-        spilletErTabt = true;
-      }
+    
+    
+    public void opdaterSynligtOrd() {
+        synligtOrd = "";
+        spilletErVundet = true;
+        for (int n = 0; n < ordet.length(); n++) {
+            String bogstav = ordet.substring(n, n + 1);
+            if (brugteBogstaver.contains(bogstav)) {
+                synligtOrd = synligtOrd + bogstav;
+            } else {
+                synligtOrd = synligtOrd + "*";
+                spilletErVundet = false;
+            }
+        }
     }
-    opdaterSynligtOrd();
-  }
-
-  public void logStatus() {
-    System.out.println("---------- ");
-    System.out.println("- ordet (skult) = " + ordet);
-    System.out.println("- synligtOrd = " + synligtOrd);
-    System.out.println("- forkerteBogstaver = " + antalForkerteBogstaver);
-    System.out.println("- brugeBogstaver = " + brugteBogstaver);
-    if (spilletErTabt) System.out.println("- SPILLET ER TABT");
-    if (spilletErVundet) System.out.println("- SPILLET ER VUNDET");
-    System.out.println("---------- ");
-  }
-  
-  public Boolean login(String username, String password) throws RemoteException{
-      Brugeradmin ba;
-      try {
-          URL url = new URL("http://javabog.dk:9901/brugeradmin?wsdl");
-			QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
-			Service service = Service.create(url, qname);
-			ba = service.getPort(Brugeradmin.class);
-		} catch (MalformedURLException e1) {
-			e1.printStackTrace();
-			return false;
-		}
-		try{
-			ba.hentBruger(username, password);
-			return true;
-			
-		} catch(IllegalArgumentException e){
-			e.printStackTrace();
-		}
-		return false;
-  }
-
+    
+    public void gætBogstav(String bogstav) {
+        if (bogstav.length() != 1) return;
+        System.out.println("Der gættes på bogstavet: " + bogstav);
+        if (brugteBogstaver.contains(bogstav)) return;
+        if (spilletErVundet || spilletErTabt) return;
+        
+        brugteBogstaver.add(bogstav);
+        
+        if (ordet.contains(bogstav)) {
+            sidsteBogstavVarKorrekt = true;
+            System.out.println("Bogstavet var korrekt: " + bogstav);
+            points++;
+        } else {
+            // Vi gættede på et bogstav der ikke var i ordet.
+            sidsteBogstavVarKorrekt = false;
+            System.out.println("Bogstavet var IKKE korrekt: " + bogstav);
+            antalForkerteBogstaver = antalForkerteBogstaver + 1;
+            if (antalForkerteBogstaver > 6) {
+                spilletErTabt = true;
+            }
+        }
+        opdaterSynligtOrd();
+    }
+    
+    public void logStatus() {
+        System.out.println("---------- ");
+        System.out.println("- ordet (skult) = " + ordet);
+        System.out.println("- synligtOrd = " + synligtOrd);
+        System.out.println("- forkerteBogstaver = " + antalForkerteBogstaver);
+        System.out.println("- brugeBogstaver = " + brugteBogstaver);
+        if (spilletErTabt) System.out.println("- SPILLET ER TABT");
+        if (spilletErVundet) System.out.println("- SPILLET ER VUNDET");
+        System.out.println("---------- ");
+    }
+    
+    
+    
+    public String hentUrl(String url) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+            StringBuilder sb = new StringBuilder();
+            String linje = br.readLine();
+            while (linje != null) {
+                sb.append(linje + "\n");
+                linje = br.readLine();
+            }
+            return sb.toString();
+        } catch (IOException ex) {
+            Logger.getLogger(GalgelogikImpl.class.getName()).log(Level.SEVERE, "Error reading from URL", ex);
+        }
+        return "Error";
+    }
+    
+    public void getWordsFromDR() {
+        String data = hentUrl("https://dr.dk");
+        data = data.substring(data.indexOf("<body")). // fjern headere
+                replaceAll("<.+?>", " ").toLowerCase(). // fjern tags
+                replaceAll("&#198;", "æ"). // erstat HTML-tegn
+                replaceAll("&#230;", "æ"). // erstat HTML-tegn
+                replaceAll("&#216;", "ø"). // erstat HTML-tegn
+                replaceAll("&#248;", "ø"). // erstat HTML-tegn
+                replaceAll("&oslash;", "ø"). // erstat HTML-tegn
+                replaceAll("&#229;", "å"). // erstat HTML-tegn
+                replaceAll("[^a-zæøå]", " "). // fjern tegn der ikke er bogstaver
+                replaceAll(" [a-zæøå] "," "). // fjern 1-bogstavsord
+                replaceAll(" [a-zæøå][a-zæøå] "," "); // fjern 2-bogstavsord
+        System.out.println("data = " + data);
+        System.out.println("data = " + Arrays.asList(data.split("\\s+")));
+        muligeOrd.clear();
+        muligeOrd.addAll(new HashSet<String>(Arrays.asList(data.split(" "))));
+        //Fjernet element på index 0 da dette er " ".
+        muligeOrd.remove(0);
+        //Clear pointcounter:
+        clearPoints();
+        System.out.println("muligeOrd = " + muligeOrd);
+        nulstil();
+    }
+    
+    
+    public Boolean login(String username, String password) throws RemoteException{
+        Brugeradmin ba;
+        try {
+            URL url = new URL("http://javabog.dk:9901/brugeradmin?wsdl");
+            QName qname = new QName("http://soap.transport.brugerautorisation/", "BrugeradminImplService");
+            Service service = Service.create(url, qname);
+            ba = service.getPort(Brugeradmin.class);
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+            return false;
+        }
+        try{
+            ba.hentBruger(username, password);
+            return true;
+            
+        } catch(IllegalArgumentException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+    
 }
